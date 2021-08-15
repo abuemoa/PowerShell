a56dd2a44264fbba1ccf8b63f3b0877037cf52f0
abuemoa <88618807+abuemoa@users.noreply.github.com>
2021-08-15 09:57:47 +0200

    Simplification of code
    Deleted unnecessary variables and added .Count to the variable $nameExe

--- OpenExe.ps1
+++ OpenExe.ps1
@@ -1,7 +1,5 @@
 $nameExe=@(Get-ChildItem | Where-Object -property name -Like "*.exe" | Get-ChildItem -name)
-$numProgramsExe=$nameExe | Measure-Object -Line | Select-Object -Expand Lines
-
-for ($i = 1; $i -lt $numProgramsExe; $i++)
+for ($i = 1; $i -lt $nameExe.Count; $i++)
 { 
     Start-Process -filepath $nameExe[$i] -ArgumentList '/silent', '/install' -Wait
     Write-output "instalando" $nameExe[$i]
